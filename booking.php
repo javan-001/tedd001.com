026a3c9efc4730d0370bb657df76514dc24b3d08
gideon <gidionkorir4@gmail.com>
2022-12-26 03:00:50 +0300

    commit

---
+++ booking.php
@@ -0,0 +1,354 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <title>Tedd Online Shopping</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-jwJxtBGVhjKkjmFpV7WZgOJIOKjY9c1zv2bWOjK0TwT2TJcPDA+0t+NhqKp3qB9+sL/f8WzjTvn8ZuxrJTQQ==" crossorigin="anonymous" />
+
+</head>
+<body>  
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Tedd Online Shopping</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          categories
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="# nature">Furniture</a>
+          <div class="dropdown-divider"></div> 
+          <a class="dropdown-item" href="#architecture">Game equipment</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#travel">Accessories</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#travel">Pets </a>
+          <div class="dropdown-divider"></div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contact">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">About</a>
+      </li>
+    </ul>
+   
+  </div>
+</nav>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/13.jpeg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/14.jpeg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/15.jpeg" alt="Third slide">
+    </div>
+      <div class="carousel-item">
+      <img class="d-block w-100" src="images/16.jpeg" alt="Fourth slide">
+      </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+ 
+
+
+
+<a id = "furniture">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">Furniture</h2>
+</div>
+
+<div class="container-fluid">
+ <div class="row">
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/3.jpeg" class="img-fluid pb-3">
+</div>
+  <div class="col-lg-4 col-md-4 col-12">
+   <img src="images/4.jpeg" class="img-fluid pb-3">
+</div>
+  <div class="col-lg-4 col-md-4 col-12">
+   <img src="images/5.jpeg" class="img-fluid pb-3">
+   </div>
+</div>
+</div>
+</section>
+</a>
+
+
+<a id = "game equipment">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">Game equipment</h2>
+</div>
+
+<div class="container-fluid">
+ <div class="row">
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/9.jpeg" class="img-fluid pb-3">
+</div>
+  <div class="col-lg-4 col-md-4 col-12">
+  <img src="images/10.jpeg" class="img-fluid pb-3">
+</div>
+ <div class="col-lg-4 col-md-4 col-12">
+  <img src="images/11.jpeg" class="img-fluid pb-3">
+  </div>
+</div>
+</div>
+</section>
+</a>
+
+
+<a id = "accessories">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">Accessories</h2>
+</div>
+
+<div class="container-fluid">
+ <div class="row">
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/6.jpeg" class="img-fluid pb-3">
+</div>
+  <div class="col-lg-4 col-md-4 col-12">
+  <img src="images/7.jpeg" class="img-fluid pb-3">
+</div>
+ <div class="col-lg-4 col-md-4 col-12">
+  <img src="images/8.jpeg" class="img-fluid pb-3">
+  </div>
+</div>
+</div>
+</section>
+</a>
+<a id = "pets">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">Pets</h2>
+</div>
+
+<div class="container-fluid">
+ <div class="row">
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/1.jpeg" class="img-fluid pb-3">
+</div>
+  <div class="col-lg-4 col-md-4 col-12">
+  <img src="images/2.jpeg" class="img-fluid pb-3">
+</div>
+ <div class="col-lg-4 col-md-4 col-12">
+  <img src="images/12.jpeg" class="img-fluid pb-3">
+  </div>
+</div>
+</div>
+</section>
+</a>
+
+<a id = "contact">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">Contact Us</h2>
+</div> 
+
+<div class="w-50 m-auto">
+   
+    <style>
+      .instagram-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 200px;
+        height: 50px;
+        margin: auto;
+        text-align: center;
+        border: 2px solid #c13584;
+        border-radius: 30px;
+      }
+
+      .instagram-link {
+        color: #c13584;
+        text-decoration: none;
+      }
+
+      .instagram-logo {
+        font-size: 24px;
+        margin-right: 10px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="instagram-container">
+      <a href="https://www.instagram.com/" class="instagram-link">
+        <i class="fab fa-instagram instagram-logo"></i>
+      </a>
+    </div>
+  </body>
+  <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+      .whatsapp-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 200px;
+        height: 50px;
+        margin: auto;
+        text-align: center;
+        border: 2px solid #25d366;
+        border-radius: 25px;
+      }
+
+      .whatsapp-link {
+        color: #25d366;
+        text-decoration: none;
+      }
+
+      .whatsapp-logo {
+        font-size: 24px;
+        margin-right: 10px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="whatsapp-container">
+      <a href="https://web.whatsapp.com/" class="whatsapp-link">
+        <i class="fab fa-whatsapp whatsapp-logo"></i>
+
+      </a>
+    </div>
+  </body>
+
+  <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+      .facebook-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 200px;
+        height: 50px;
+        margin: auto;
+        text-align: center;
+        border: 2px solid #3b5998;
+        border-radius: 25px;
+      }
+
+      .facebook-link {
+        color: #3b5998;
+        text-decoration: none;
+      }
+
+      .facebook-logo {
+        font-size: 24px;
+        margin-right: 10px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="facebook-container">
+      <a href="https://www.facebook.com/" class="facebook-link">
+        <i class="fab fa-facebook-f facebook-logo"></i>
+      </a>
+    </div>
+  </body>
+  <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+      .gmail-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 200px;
+        height: 50px;
+        margin: auto;
+        text-align: center;
+        border: 2px solid #dd4b39;
+        border-radius: 25px;
+      }
+
+      .gmail-link {
+        color: #3e65cf,#c71610,#f2a60c,#3b60c4,#08851b;
+        text-decoration: none;
+      }
+
+      .gmail-logo {
+        font-size: 24px;
+        margin-right: 10px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="gmail-container">
+      <a href="https://mail.google.com/mail/" class="gmail-link">
+        <i class="fab fa-google gmail-logo"></i>
+      </a>
+    </div>
+  </body>
+
+
+
+
+
+
+
+
+
+<a id = "about">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">About Us</h2>
+</div>
+<div class="container-fluid">
+    <h2 class="text-center">Tedd Online Shopping</h2>
+    <p class="text-center">
+      <b>We are passionate online traders dealing with furniture,game equipments,accessories and pets.
+      We are full time traders with an experience of 5 years.
+      We are located at Midway Street 23,Topnotch building,room 3,Fifth floor.
+</b></p>
+
+</div>
+</a>
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">Our Mission</h2>
+    <p class="text-center">
+      <b>Our mission is to make Target your preferred shopping destination in all channels by delivering outstanding value, continuous innovation and exceptional guest experiences by consistently fulfilling our Expect More. Pay Less. Brand Promise.
+</b></p>
+
+</div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
